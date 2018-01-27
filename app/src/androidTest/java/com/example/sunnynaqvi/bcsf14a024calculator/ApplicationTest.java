318c7ebf597443504ae6b2dbf2fec19515ce2349
SunnyNaqvi <sunnybaba51214@gmail.com>
2018-01-27 23:40:14 +0500

    First commit

---
+++ app/src/androidTest/java/com/example/sunnynaqvi/bcsf14a024calculator/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.sunnynaqvi.bcsf14a024calculator;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
